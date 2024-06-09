74a095b116905d99b8819cf6b859ebd116b5eaac
Elkin <elkin148@hotmail.com>
2024-06-09 16:17:54 -0500

    se garegan modificaciones de ruta

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://elkin-torres.github.io/TallerSemana4/",
+  base: "/TallerSemana4/",
   plugins: [react()],
 });
