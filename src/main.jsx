66b29814d3a571fee092d8fed10688212e381fd5
Elkin <elkin148@hotmail.com>
2024-06-09 18:00:22 -0500

    se corrige basename

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,11 @@ import { Provider } from "react-redux";
 import "./style/styles.css";
 import { BrowserRouter } from "react-router-dom";
 import MyRouter from "./router/MyRouter";
-import {store} from "./store";
+import { store } from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store} >
+    <Provider store={store}>
       <BrowserRouter>
         <MyRouter />
       </BrowserRouter>
