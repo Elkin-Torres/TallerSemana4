bc9fc57286685960df0e9dfe313264617dd14071
Elkin <elkin148@hotmail.com>
2024-06-09 16:05:46 -0500

    se modifican rutas

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 const Welcome = () => {
   return (
     <div>
-      <div>Welcome</div>
+      <div>Welcome!</div>
       <Link to={"/bill"}>
-        <button>Registrar factura</button>
+        <button>-Registrar factura-</button>
       </Link>
     </div>
   );
